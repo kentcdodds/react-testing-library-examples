74a425e70f39939d6339bf96a43289f2462a6279
Kent C. Dodds <kent+github@doddsfamily.us>
2018-06-04 14:58:22 -0600

    updates

--- src/index.js
+++ src/index.js
@@ -1,20 +1,35 @@
 import React from 'react'
 import {render} from 'react-dom'
 
-const App = () => (
-  <div>
-    <h1>react-testing-library examples 🐐</h1>
+function App() {
+  return (
     <div>
-      <p>{`
-        This is an example project of how to test react components
-        using react-testing-library and Jest. The idea is that most
-        of the tests should be runnable in codesandbox to make it
-        easy to try things out, fork, contribute, etc. However,
-        there will be some tests which do not work in codesandbox
-        and we'll put those in a special folder called __local_tests__.
-      `}</p>
+      <h1>react-testing-library examples 🐐</h1>
+      <div>
+        <p>{`
+          This is an example project of how to test react components
+          using react-testing-library and Jest. The idea is that most
+          of the tests should be runnable in codesandbox to make it
+          easy to try things out, fork, contribute, etc. However,
+          there will be some tests which do not work in codesandbox
+          and we'll put those in a special folder called __local_tests__.
+        `}</p>
+        <p>{`
+        `}</p>
+        <p>
+          <a href="https://codesandbox.io/s/github/kentcdodds/react-testing-library-examples">
+            Open this in codesandbox
+          </a>{' '}
+          to play around with things. You can also use the codesandbox to
+          contribute to{' '}
+          <a href="https://github.com/kentcdodds/react-testing-library-examples">
+            the project on GitHub
+          </a>
+          !
+        </p>
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 render(<App />, document.getElementById('root'))
