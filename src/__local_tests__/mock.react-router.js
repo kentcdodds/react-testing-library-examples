debf78f266951cf93284e0cfd3588b3b19f27588
Kent C. Dodds <kent@doddsfamily.us>
2018-06-22 10:58:55 -0600

    fixup some things

--- src/__local_tests__/mock.react-router.js
+++ src/__local_tests__/mock.react-router.js
@@ -1,15 +1,16 @@
 import 'jest-dom/extend-expect'
+import 'react-testing-library/cleanup-after-each'
 import React from 'react'
 import {withRouter} from 'react-router'
-import {render, fireEvent, cleanup} from 'react-testing-library'
-
-afterEach(cleanup)
+import {render} from 'react-testing-library'
 
 // pretend this is in another file, and we:
 // import {LocationDisplay} from './location-display'
 const LocationDisplay = withRouter(({location}) => (
   <div data-testid="location-display">{location.pathname}</div>
 ))
+
+// your tests should look like this below:
 
 jest.mock('react-router', () => ({
   withRouter: Comp => props => <Comp {...props} />,
