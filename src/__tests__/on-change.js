ac343e7ffb741e3a6df99c6c27cbd8f18e1a08cf
Kent C. Dodds <kent@doddsfamily.us>
2018-08-09 10:46:25 -0600

    improve on-change example

--- src/__tests__/on-change.js
+++ src/__tests__/on-change.js
@@ -1,52 +1,40 @@
+import 'react-testing-library/cleanup-after-each'
 import React from 'react'
-import {render, fireEvent, cleanup} from 'react-testing-library'
+import {render, fireEvent} from 'react-testing-library'
 
-afterEach(cleanup)
+class UpperInput extends React.Component {
+  state = {upper: ''}
 
-class SignIn extends React.Component {
-  state = {email: '', password: ''}
-
-  onEmailChange = e => {
-    this.setState({email: e.target.value})
-  }
-
-  onPasswordChange = e => {
-    this.setState({password: e.target.value})
+  onUpperChange = e => {
+    this.setState({upper: e.target.value.toUpperCase()})
   }
 
   render() {
     return (
-      <form onSubmit={this.onSubmit}>
-        <label htmlFor="email">Email</label>
+      <div>
+        <label htmlFor="upper">Upper</label>
         <input
-          type="email"
-          id="email"
-          value={this.state.email}
-          onChange={this.onEmailChange}
+          id="upper"
+          value={this.state.upper}
+          onChange={this.onUpperChange}
         />
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          id="password"
-          value={this.state.password}
-          onChange={this.onPasswordChange}
-        />
-      </form>
+      </div>
     )
   }
 }
 
-test('SignIn form email and password fields should change', () => {
-  const {getByLabelText} = render(<SignIn />)
-  const emailInput = getByLabelText('Email')
-  const passwordInput = getByLabelText('Password')
-  // due to the way React tracks input values, you cannot
+test('sets the value to the upper version of the value', () => {
+  const {getByLabelText} = render(<UpperInput />)
+  const upperInput = getByLabelText(/upper/i)
+  const upper = 'stuff'
+  // this is how you might be used to doing this to change the input value:
+  // upperInput.value = upper
+  // fireEvent.change(upperInput)
+  // However due to the way React tracks input values, you cannot
   // simply set the `.value` property of an input node and fire a change event
   // so fireEvent allows you to specify properties you want applied to the
   // target in a way that side-step's React's tracking so you can correctly
   // test the onChange handler's behavior
-  fireEvent.change(emailInput, {target: {value: 'test@test.com'}})
-  fireEvent.change(passwordInput, {target: {value: '12345678'}})
-  expect(emailInput.value).toEqual('test@test.com')
-  expect(passwordInput.value).toEqual('12345678')
+  fireEvent.change(upperInput, {target: {value: upper}})
+  expect(upperInput.value).toEqual(upper.toUpperCase())
 })
