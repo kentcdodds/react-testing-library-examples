a3a7a00a4d20fd7e07acdd7706491009f3296614
Kent C. Dodds <kent@doddsfamily.us>
2018-08-22 21:55:48 -0600

    update upload file example

--- src/__tests__/upload-file.js
+++ src/__tests__/upload-file.js
@@ -38,31 +38,12 @@ class UploadFile extends Component {
 
 test('Show the uploaded file name after the user uploads a file', () => {
   const {getByLabelText, getByText, container} = render(<UploadFile />)
+  const inputEl = getByLabelText(/upload file/i)
 
   const file = new File(['(⌐□_□)'], 'chucknorris.png', {
     type: 'image/png',
   })
-
-  const inputEl = getByLabelText('Upload File')
-
-  // input.files is a read-only property
-  // so this is not allowed
-  // input.files = [file]
-
-  // But DOM properties are reconfigurable
-  // I got this while reading through a related JSDOM Github issue
-  // https://github.com/jsdom/jsdom/issues/1272#issuecomment-150670691
-  Object.defineProperty(inputEl, 'files', {
-    value: [file],
-  })
-
-  // If you want to trigger the onChange handler of a controlled component
-  // with a different event.target.value, sending value through
-  // eventProperties won't work like it does with Simulate.
-  // You need to change the element's value property,
-  // then use fireEvent to fire a change DOM event.
-  // https://github.com/kentcdodds/react-testing-library#fireeventeventnamenode-htmlelement-eventproperties-object
-  fireEvent.change(inputEl)
+  fireEvent.change(inputEl, {target: {files: [file]}})
 
   expect(container).toContainElement(getByText(/chucknorris\.png/))
 })
