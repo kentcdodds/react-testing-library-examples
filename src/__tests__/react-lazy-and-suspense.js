ae7494bf8cd5a30dde5154835e6cb3b355556362
Kent C. Dodds <kent@doddsfamily.us>
2018-12-12 10:12:59 -0700

    suspense + react.lazy example

---
+++ src/__tests__/react-lazy-and-suspense.js
@@ -0,0 +1,44 @@
+import 'jest-dom/extend-expect'
+import React from 'react'
+import {render, waitForElement, cleanup} from 'react-testing-library'
+
+afterEach(cleanup)
+
+const LazyComponent = React.lazy(() => import('../lazy-component'))
+
+function Main() {
+  return (
+    <div>
+      <div>Lazy loaded component is here:</div>
+      <LazyComponent />
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <React.Suspense fallback="loading...">
+      <Main />
+    </React.Suspense>
+  )
+}
+
+test('renders lazy', async () => {
+  // this is how you test a component that needs a suspense component
+  // just render it with your own suspense!
+  const {getByText} = render(
+    <React.Suspense fallback="test loading">
+      <Main />
+    </React.Suspense>,
+  )
+  const lazyElement = await waitForElement(() => getByText(/i am lazy/i))
+  expect(lazyElement).toBeInTheDocument()
+})
+
+test('app renders stuff!', async () => {
+  // this is how you render a component that has it's own suspense.
+  // no need to render our own suspense in the test.
+  const {getByText} = render(<App />)
+  const lazyElement = await waitForElement(() => getByText(/i am lazy/i))
+  expect(lazyElement).toBeInTheDocument()
+})
