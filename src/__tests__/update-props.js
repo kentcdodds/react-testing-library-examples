7593aeb0d096917d01ac2f9d64b0b4b4193cf777
Michaël De Boey <info@michaeldeboey.be>
2020-09-16 17:46:30 +0200

    Update update-props.js (#19)

--- src/__tests__/update-props.js
+++ src/__tests__/update-props.js
@@ -2,21 +2,20 @@
 // the basic idea is to simply call `render` again and provide the same container
 // that your first call created for you.
 
-import React from 'react'
+import React, {useRef} from 'react'
 import {render, screen} from '@testing-library/react'
 
 let idCounter = 1
 
-class NumberDisplay extends React.Component {
-  id = idCounter++ // to ensure we don't remount a different instance
-  render() {
-    return (
-      <div>
-        <span data-testid="number-display">{this.props.number}</span>
-        <span data-testid="instance-id">{this.id}</span>
-      </div>
-    )
-  }
+const NumberDisplay = ({number}) => {
+  const id = useRef(idCounter++) // to ensure we don't remount a different instance
+  
+  return (
+    <div>
+      <span data-testid="number-display">{number}</span>
+      <span data-testid="instance-id">{id.current}</span>
+    </div>
+  )
 }
 
 test('calling render with the same component on the same container does not remount', () => {
