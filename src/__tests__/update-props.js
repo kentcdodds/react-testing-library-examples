5bf24657f145ed1c5be1e00b5065122f8c41503d
Michaël De Boey <info@michaeldeboey.be>
2020-09-24 19:07:16 +0200

    Update update-props.js

--- src/__tests__/update-props.js
+++ src/__tests__/update-props.js
@@ -9,7 +9,7 @@ let idCounter = 1
 
 const NumberDisplay = ({number}) => {
   const id = useRef(idCounter++) // to ensure we don't remount a different instance
-  
+
   return (
     <div>
       <span data-testid="number-display">{number}</span>
@@ -20,11 +20,11 @@ const NumberDisplay = ({number}) => {
 
 test('calling render with the same component on the same container does not remount', () => {
   const {rerender} = render(<NumberDisplay number={1} />)
-  expect(screen.getByTestId('number-display').textContent).toBe('1')
+  expect(screen.getByTestId('number-display')).toHaveTextContent('1')
 
   // re-render the same component with different props
   rerender(<NumberDisplay number={2} />)
-  expect(screen.getByTestId('number-display').textContent).toBe('2')
+  expect(screen.getByTestId('number-display')).toHaveTextContent('2')
 
-  expect(screen.getByTestId('instance-id').textContent).toBe('1')
+  expect(screen.getByTestId('instance-id')).toHaveTextContent('1')
 })
