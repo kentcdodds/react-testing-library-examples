24ca6ac214d1039c8623f223807216aa99351d1e
Michaël De Boey <info@michaeldeboey.be>
2020-09-24 17:48:25 +0200

    Update react-router.js

--- src/__tests__/react-router.js
+++ src/__tests__/react-router.js
@@ -1,4 +1,4 @@
-import {render, screen} from '@testing-library/react'
+import {render as rtlRender, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import React from 'react'
 import {
@@ -47,29 +47,29 @@ const App = () => (
 
 // this is a handy function that I would utilize for any component
 // that relies on the router being in context
-const renderWithRouter = (ui, {route = '/'} = {}) => {
+const render = (ui, {route = '/'} = {}) => {
   window.history.pushState({}, 'Test page', route)
 
-  return render(ui, {wrapper: Router})
+  return rtlRender(ui, {wrapper: Router})
 }
 
 test('full app rendering/navigating', () => {
   renderWithRouter(<App />)
   // normally I'd use a data-testid, but just wanted to show this is also possible
-  expect(screen.getByText(/you are home/)).toBeInTheDocument()
+  expect(screen.getByText(/you are home/i)).toBeInTheDocument()
 
   const leftClick = {button: 0}
   userEvent.click(screen.getByText(/about/i), leftClick)
 
   // normally I'd use a data-testid, but just wanted to show this is also possible
-  expect(screen.getByText(/you are on the about page/)).toBeInTheDocument()
+  expect(screen.getByText(/you are on the about page/i)).toBeInTheDocument()
 })
 
 test('landing on a bad page', () => {
   renderWithRouter(<App />, {route: '/something-that-does-not-match'})
 
   // normally I'd use a data-testid, but just wanted to show this is also possible
-  expect(screen.getByText(/no match/)).toBeInTheDocument()
+  expect(screen.getByText(/no match/i)).toBeInTheDocument()
 })
 
 test('rendering a component that uses withRouter', () => {
