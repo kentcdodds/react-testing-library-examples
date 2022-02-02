83557d442a6ada7ef5566b6017b1f026475a78bc
Ken Rimple <krimple@chariotsolutions.com>
2022-02-02 15:38:28 -0500

    fixed async test to wait until render is finished to end, and removed .skip on date test which passes (correctly?)

--- src/__tests__/async.js
+++ src/__tests__/async.js
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import {rest} from 'msw'
 import {setupServer} from 'msw/node'
-import {render, screen} from '@testing-library/react'
+import {render, screen, waitFor} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom/extend-expect'
 import {FetchGreeting} from '../fetch-greeting'
@@ -26,18 +26,20 @@ test('loads and displays greeting', async () => {
   expect(screen.getByRole('button')).toHaveAttribute('disabled')
 })
 
-test('handles server error', async () => {
-  server.use(
-    rest.get('/greeting', (req, res, ctx) => {
-      return res(ctx.status(500))
-    }),
-  )
+test('handles server error',
+  async () => {
+    server.use(
+      rest.get('/greeting', (req, res, ctx) => {
+        return res(ctx.status(500))
+      }),
+    )
 
-  render(<FetchGreeting />)
+    render(<FetchGreeting />)
 
-  userEvent.click(screen.getByText('Load Greeting'))
+    userEvent.click(screen.getByText('Load Greeting'))
 
-  await screen.findByRole('alert', {name: 'Oops, failed to fetch!'})
+    await screen.queryByRole('alert', {name: 'Oops, failed to fetch!'})
 
-  expect(screen.getByRole('button')).not.toHaveAttribute('disabled')
-})
+    await waitFor(() => { expect(screen.getByRole('button')).toBeInTheDocument() })
+    await waitFor(() => { expect(screen.getByRole('button')).not.toHaveAttribute('disabled') })
+  })
