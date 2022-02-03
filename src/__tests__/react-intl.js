fcc50385994717b9ca34fc33cab54d2b44dcbd6b
Ken Rimple <krimple@chariotsolutions.com>
2022-02-03 13:37:26 -0500

    fixed async test to wait until render is finished to end, and removed .skip on date test which passes (correctly?) (#31)

--- src/__tests__/react-intl.js
+++ src/__tests__/react-intl.js
@@ -29,7 +29,7 @@ function render(ui, options) {
 // this should work, but for some reason it's not able to load the locale
 // even though we have the IntlPolyfill installed
 // I promise it is. Just try console.log(global.IntlPolyfill)
-test.skip('it should render FormattedDate and have a formated pt date', () => {
+test('it should render FormattedDate and have a formated pt date', () => {
   render(<FormatDateView />)
   expect(screen.getByTestId('date-display')).toHaveTextContent('11/03/2019')
 })
